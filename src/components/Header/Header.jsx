649f1a25c8c1300922a847007b06d5cecaaa0696
maysaraujo <maysaaraujo25@gmail.com>
2024-05-06 12:31:32 -0300

    first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import Dogs from '../../assets/dogs.svg?react';
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to='/' aria-label='Dogs - Home'>
+          <Dogs />
+        </Link>
+        <Link className={styles.login} to='/login'>
+          Login / Criar
+        </Link>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
