9b775682413631b0cf55a763509f7ed0f31323ba
maysaraujo <maysaaraujo25@gmail.com>
2024-05-08 18:10:32 -0300

    fix: adding UserContext and Navigate component to Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,10 +1,16 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
+import { useContext } from 'react';
+import { UserContext } from '../../UserContext';
 
 const Login = () => {
+  const { login } = useContext(UserContext);
+
+  if (login === true) return <Navigate to='/conta' />;
+
   return (
     <div>
       <Routes>
