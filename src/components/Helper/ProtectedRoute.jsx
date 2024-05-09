71a5f3fa738860f52e2560ff39182ac45d0b3fc1
maysaraujo <maysaaraujo25@gmail.com>
2024-05-09 12:18:37 -0300

    feat: creating functionality of ProtectedRoute

---
+++ src/components/Helper/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { useContext } from 'react';
+import { UserContext } from '../../UserContext';
+import { Navigate } from 'react-router-dom';
+
+const ProtectedRoute = ({ children }) => {
+  const { login } = useContext(UserContext);
+
+  if (login === true) {
+    return children;
+  } else if (login === false) {
+    return <Navigate to='/login' />;
+  } else {
+    return <></>;
+  }
+};
+
+export default ProtectedRoute;
