5cb92bdbda6c6d42b9461f6c55663ed39a3fdcfc
maysaraujo <maysaaraujo25@gmail.com>
2024-05-09 11:59:50 -0300

    feat: adding error functionality to login

--- src/components/Login/LoginCreate.jsx
+++ src/components/Login/LoginCreate.jsx
@@ -4,6 +4,8 @@ import useForm from '../../hooks/useForm';
 import { USER_POST } from '../../Api';
 import { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import useFetch from '../../hooks/useFetch';
+import Error from '../Helper/Error';
 
 const LoginCreate = () => {
   const username = useForm();
@@ -11,6 +13,7 @@ const LoginCreate = () => {
   const password = useForm();
 
   const { userLogin } = useContext(UserContext);
+  const { loading, error, request } = useFetch();
 
   async function handleSubmit(event) {
     event.preventDefault();
@@ -20,8 +23,7 @@ const LoginCreate = () => {
       email: email.value,
       password: password.value,
     });
-    const response = await fetch(url, options);
-
+    const { response } = await request(url, options);
     if (response.ok) userLogin(username.value, password.value);
   }
 
@@ -32,7 +34,12 @@ const LoginCreate = () => {
         <Input label='Usuário' type='text' name='username' {...username} />
         <Input label='Email' type='email' name='email' {...email} />
         <Input label='Senha' type='password' name='password' {...password} />
-        <Button>Cadastrar</Button>
+        {loading ? (
+          <Button disabled>Cadastrando...</Button>
+        ) : (
+          <Button>Cadastrar</Button>
+        )}
+        <Error error={error} />
       </form>
     </section>
   );
