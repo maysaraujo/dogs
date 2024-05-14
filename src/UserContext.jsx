ac2b2e59c337e52ff1604b74c14604005c2cf2d2
maysaraujo <maysaaraujo25@gmail.com>
2024-05-14 16:22:04 -0300

    fix: fixing useNavigate version bug

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useCallback, useEffect, useState } from 'react';
 import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './Api';
-import { useNavigate } from 'react-router-dom';
 
 export const UserContext = createContext();
 
@@ -9,20 +8,15 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
-  const navigate = useNavigate();
 
-  const userLogout = useCallback(
-    async function() {
-      setData(null);
-      setError(null);
-      setLoading(false);
-      setLogin(false);
+  const userLogout = useCallback(async function() {
+    setData(null);
+    setError(null);
+    setLoading(false);
+    setLogin(false);
 
-      window.localStorage.removeItem('token');
-      navigate('/login');
-    },
-    [navigate]
-  );
+    window.localStorage.removeItem('token');
+  }, []);
 
   async function getUser(token) {
     const { url, options } = USER_GET(token);
