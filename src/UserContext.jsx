35b558a32fb74163da2a19d020376e3b1b9a1576
maysaraujo <maysaaraujo25@gmail.com>
2024-05-14 17:09:30 -0300

    fix: fixing bug of useNavigate

--- src/UserContext.jsx
+++ src/UserContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useCallback, useEffect, useState } from 'react';
 import { TOKEN_POST, TOKEN_VALIDATE_POST, USER_GET } from './Api';
+import { useNavigate } from 'react-router-dom';
 
 export const UserContext = createContext();
 
@@ -8,6 +9,7 @@ export const UserStorage = ({ children }) => {
   const [login, setLogin] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const userLogout = useCallback(async function() {
     setData(null);
