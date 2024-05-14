a8521d6a348d77db0c33a58cf47f726c26c304fc
maysaraujo <maysaaraujo25@gmail.com>
2024-05-14 16:17:00 -0300

    feat: updating User component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from 'react-router-dom';
+import UserHeader from './UserHeader/UserHeader';
+import Feed from '../Feed/Feed';
+import UserPhotoPost from './UserPhotoPost';
+import UserStats from './UserStats';
+
 const User = () => {
   return (
-    <div>Usuário</div>
-  )
-}
+    <section className='container'>
+      <UserHeader />
+      <Routes>
+        <Route path='/' element={<Feed />} />
+        <Route path='postar' element={<UserPhotoPost />} />
+        <Route path='estatisticas' element={<UserStats />} />
+      </Routes>
+    </section>
+  );
+};
 
-export default User
+export default User;
