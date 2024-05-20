fff416e85be9ae831d11eb2336a80c32b4acb909
maysaraujo <maysaaraujo25@gmail.com>
2024-05-20 19:22:21 -0300

    feat: updating PhotoCommentsForm component

--- src/components/Photo/PhotoCommentsForm/PhotoCommentsForm.jsx
+++ src/components/Photo/PhotoCommentsForm/PhotoCommentsForm.jsx
@@ -3,6 +3,7 @@ import Enviar from '../../../assets/enviar.svg?react';
 import useFetch from '../../../hooks/useFetch';
 import { COMMENT_POST } from '../../../Api';
 import Error from '../../Helper/Error';
+import styles from './PhotoCommentsForm.module.css';
 
 const PhotoCommentsForm = ({ id, setComments }) => {
   const { request, error } = useFetch();
@@ -21,15 +22,16 @@ const PhotoCommentsForm = ({ id, setComments }) => {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <textarea
+        className={styles.textarea}
         id='comment'
         name='comment'
-        placeholder='Commente...'
+        placeholder='Comente...'
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
-      <button>
+      <button className={styles.button}>
         <Enviar />
       </button>
       <Error error={error} />
