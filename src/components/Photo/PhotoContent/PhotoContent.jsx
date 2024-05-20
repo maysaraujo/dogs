3d43fe572711d3197c0b143cdb57aeadfc9a8575
maysaraujo <maysaaraujo25@gmail.com>
2024-05-20 19:52:06 -0300

    feat: updating PhotoContent component

--- src/components/Photo/PhotoContent/PhotoContent.jsx
+++ src/components/Photo/PhotoContent/PhotoContent.jsx
@@ -5,6 +5,7 @@ import PhotoComments from '../PhotoComments/PhotoComments';
 import { useContext } from 'react';
 import { UserContext } from '../../../UserContext';
 import PhotoDelete from '../PhotoDelete/PhotoDelete';
+import Image from '../../Helper/Image/Image';
 
 const PhotoContent = ({ data }) => {
   const user = useContext(UserContext);
@@ -13,7 +14,7 @@ const PhotoContent = ({ data }) => {
   return (
     <div className={styles.photo}>
       <div className={styles.img}>
-        <img src={photo.src} alt={photo.title} />
+        <Image src={photo.src} alt={photo.title} />
       </div>
       <div className={styles.details}>
         <div className=''>
