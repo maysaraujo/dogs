1ebc59fc9688613343baa7b5bfe3bf683c1fb661
maysaraujo <maysaaraujo25@gmail.com>
2024-05-20 19:39:32 -0300

    feat: adding condition to delete photo

--- src/components/Photo/PhotoContent/PhotoContent.jsx
+++ src/components/Photo/PhotoContent/PhotoContent.jsx
@@ -1,8 +1,13 @@
+/* eslint-disable react/prop-types */
 import { Link } from 'react-router-dom';
 import styles from './PhotoContent.module.css';
 import PhotoComments from '../PhotoComments/PhotoComments';
+import { useContext } from 'react';
+import { UserContext } from '../../../UserContext';
+import PhotoDelete from '../PhotoDelete/PhotoDelete';
 
 const PhotoContent = ({ data }) => {
+  const user = useContext(UserContext);
   const { photo, comments } = data;
 
   return (
@@ -13,7 +18,11 @@ const PhotoContent = ({ data }) => {
       <div className={styles.details}>
         <div className=''>
           <p className={styles.author}>
-            <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            {user.data && user.data.username === photo.author ? (
+              <PhotoDelete id={photo.id} />
+            ) : (
+              <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>
+            )}
             <span className={styles.visualizacoes}>{photo.acessos}</span>
           </p>
           <h1 className='title'>
