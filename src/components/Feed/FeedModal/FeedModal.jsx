924f0cdc3a4f94de65aa5d0acb27cf8da127f64a
maysaraujo <maysaaraujo25@gmail.com>
2024-05-20 16:41:46 -0300

    feat: updating FeedModal component

--- src/components/Feed/FeedModal/FeedModal.jsx
+++ src/components/Feed/FeedModal/FeedModal.jsx
@@ -1,7 +1,30 @@
-const FeedModal = () => {
-  return (
-    <div>FeedModal</div>
-  )
-}
+import styles from './FeedModal.module.css';
+import useFetch from '../../../hooks/useFetch';
+import { useEffect } from 'react';
+import { PHOTO_GET } from '../../../Api';
+import Error from '../../Helper/Error';
+import Loading from '../../Helper/Loading';
+import PhotoContent from '../../Photo/PhotoContent/PhotoContent';
 
-export default FeedModal
+const FeedModal = ({ photo, setModalPhoto }) => {
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(photo.id);
+    request(url, options);
+  }, [photo, request]);
+
+  function handleOutsideClick(event) {
+    if (event.target === event.currentTarget) setModalPhoto(null);
+  }
+
+  return (
+    <div className={styles.modal} onClick={handleOutsideClick}>
+      {error && <Error error={error} />}
+      {loading && <Loading />}
+      {data && <PhotoContent data={data} />}
+    </div>
+  );
+};
+
+export default FeedModal;
