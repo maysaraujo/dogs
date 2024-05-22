3d52f3885955aa5905c4506c107d045d619a0b02
maysaraujo <maysaaraujo25@gmail.com>
2024-05-22 18:20:59 -0300

    refactor: removing console.log

--- src/components/Feed/FeedPhotos/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos/FeedPhotos.jsx
@@ -16,7 +16,6 @@ const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
       const { response, json } = await request(url, options);
 
       if (response && response.ok && json.length < total) setInfinite(false);
-      console.log('Request', json);
     }
     fetchPhotos();
   }, [request, user, page, setInfinite]);
