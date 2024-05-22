9b6255a1e8eb9e9ed194fd4f4175cc7d83ec6f12
maysaraujo <maysaaraujo25@gmail.com>
2024-05-22 12:25:56 -0300

    feat: updating FeedPhotos component

--- src/components/Feed/FeedPhotos/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos/FeedPhotos.jsx
@@ -6,18 +6,20 @@ import Error from '../../Helper/Error';
 import Loading from '../../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ page, user, setModalPhoto, setInfinite }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page, total, user });
+      const { response, json } = await request(url, options);
 
-      const { json } = await request(url, options);
-      console.log(json);
+      if (response && response.ok && json.length < total) setInfinite(false);
+      console.log('Request', json);
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
 
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
