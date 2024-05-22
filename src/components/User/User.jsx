e5bd2527394818cd7444952bb019bb08a69a528c
maysaraujo <maysaaraujo25@gmail.com>
2024-05-22 18:16:18 -0300

    feat: adding route NotFound to component

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -5,6 +5,7 @@ import UserPhotoPost from './UserPhotoPost';
 import UserStats from './UserStats';
 import { useContext } from 'react';
 import { UserContext } from '../../UserContext';
+import NotFound from '../NotFound/NotFound';
 
 const User = () => {
   const { data } = useContext(UserContext);
@@ -16,6 +17,7 @@ const User = () => {
         <Route path='/' element={<Feed user={data.id} />} />
         <Route path='postar' element={<UserPhotoPost />} />
         <Route path='estatisticas' element={<UserStats />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </section>
   );
