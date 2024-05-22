fa253f64900708097bcdb2dec6e4dcd554269ce6
maysaraujo <maysaaraujo25@gmail.com>
2024-05-22 18:06:15 -0300

    feat: updating Photo component

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,7 +1,29 @@
-const Photo = () => {
-  return (
-    <div>Photo</div>
-  )
-}
+import { useParams } from 'react-router-dom';
+import useFetch from '../../hooks/useFetch';
+import { useEffect } from 'react';
+import { PHOTO_GET } from '../../Api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import PhotoContent from '../Photo/PhotoContent/PhotoContent';
 
-export default Photo
+const Photo = () => {
+  const { id } = useParams();
+  const { data, loading, error, request } = useFetch();
+
+  useEffect(() => {
+    const { url, options } = PHOTO_GET(id);
+    request(url, options);
+  }, [request, id]);
+
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <section className='container mainContainer'>
+        <PhotoContent single={true} data={data} />
+      </section>
+    );
+  else return;
+};
+
+export default Photo;
