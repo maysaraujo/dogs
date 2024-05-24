da349fcb42ead51931f5d79903b37f5d3965b213
maysaraujo <maysaaraujo25@gmail.com>
2024-05-24 20:28:39 -0300

    feat: updating LoginPassowrdReset component

--- src/components/Login/LoginPasswordReset.jsx
+++ src/components/Login/LoginPasswordReset.jsx
@@ -1,9 +1,63 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import Input from '../Forms/Input/Input';
+import Button from '../Forms/Button/Button';
+import useForm from '../../hooks/useForm';
+import useFetch from '../../hooks/useFetch';
+import { PASSWORD_RESET } from '../../Api';
+import Error from '../Helper/Error';
+import { useNavigate } from 'react-router-dom';
 
 const LoginPasswordReset = () => {
-  return (
-    <div>LoginPasswordReset</div>
-  )
-}
+  const [login, setLogin] = useState('');
+  const [key, setkey] = useState('');
+  const password = useForm();
+  const { error, loading, request } = useFetch();
+  const navigate = useNavigate();
 
-export default LoginPasswordReset
+  useEffect(() => {
+    const params = new URLSearchParams(window.location.search);
+    const key = params.get('key');
+    const login = params.get('login');
+
+    if (key) setKey(key);
+    if (login) setLogin(login);
+  }, []);
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+
+    if (password.validate()) {
+      const { url, options } = PASSWORD_RESET({
+        login,
+        key,
+        password: password.value,
+      });
+
+      const { response } = await request(url, options);
+
+      if (response.ok) navigate('/login');
+    }
+  }
+
+  return (
+    <div>
+      <h1 className='title'>Resete a Senha</h1>
+      <form onSubmit={handleSubmit}>
+        <Input
+          label='Nova Senha'
+          type='password'
+          name='password'
+          {...password}
+        />
+        {loading ? (
+          <Button disabled>Resetando...</Button>
+        ) : (
+          <Button>Resetar</Button>
+        )}
+      </form>
+      <Error error={error} />
+    </div>
+  );
+};
+
+export default LoginPasswordReset;
