708cc94f31a778a4d9ab92355f6dca6eb5c35ee2
maysaraujo <maysaaraujo25@gmail.com>
2024-05-24 21:30:43 -0300

    feat: updating Footer and it's styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,13 @@
 import styles from './Footer.module.css';
+import Dogs from '../../assets/dogs-footer.svg?react';
 
 const Footer = () => {
-  return <div className={styles.footer}></div>;
+  return (
+    <footer className={styles.footer}>
+      <Dogs />
+      <p>Dogs. Alguns direitos reservados.</p>
+    </footer>
+  );
 };
 
 export default Footer;
