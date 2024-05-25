3bf51c1cebda4317bb1998f4377a07e80f2b75e3
maysaraujo <maysaaraujo25@gmail.com>
2024-05-25 11:06:56 -0300

    feat: updating UserStats component

--- src/components/User/UserStats.jsx
+++ src/components/User/UserStats.jsx
@@ -1,11 +1,32 @@
 import Head from '../Helper/Head';
+import useFetch from '../../hooks/useFetch';
+import { useEffect } from 'react';
+import { STATS_GET } from '../../Api';
+import Loading from '../Helper/Loading';
+import Error from '../Helper/Error';
+import UserStatsGraphs from './UserStatsGraphs/UserStatsGraphs';
 
 const UserStats = () => {
-  return (
-    <div>
-      <Head title='Estatísticas' />
-    </div>
-  );
+  const { data, error, loading, request } = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const { url, options } = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />;
+  if (error) return <Error error={error} />;
+  if (data)
+    return (
+      <div>
+        <Head title='Estatísticas' />
+        <UserStatsGraphs data={data} />
+      </div>
+    );
+  else return null;
 };
 
 export default UserStats;
