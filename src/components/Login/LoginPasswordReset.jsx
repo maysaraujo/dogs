473078b4d0f14928de9251dd2e0c105c035ed09a
maysaraujo <maysaaraujo25@gmail.com>
2024-05-25 11:25:59 -0300

    feat: adding class animeLeft to components

--- src/components/Login/LoginPasswordReset.jsx
+++ src/components/Login/LoginPasswordReset.jsx
@@ -41,7 +41,7 @@ const LoginPasswordReset = () => {
   }
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title='Resete a Senha' />
       <h1 className='title'>Resete a Senha</h1>
       <form onSubmit={handleSubmit}>
@@ -58,7 +58,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
